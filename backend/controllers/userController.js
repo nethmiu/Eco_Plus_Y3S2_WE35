c61e4c2828f6fbddb59214fb9d661335b74647a5
Nethmi Umaya Silva <nethmiumayapc@gmail.com>
2025-08-22 11:36:06 +0530

    Add backend part of Registation and login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// JWT Token generation function
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// @desc    Register a new user
+// @route   POST /api/users/register
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ message: 'This Email is already registered' });
+      return;
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: 'User data is invalid' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error: ' + error.message });
+  }
+};
+
+// @desc    Auth user & get token (Login)
+// @route   POST /api/users/login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Email or Password is incorrect' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error: ' + error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
